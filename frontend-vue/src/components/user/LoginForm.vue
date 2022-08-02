88e6e21894c599eb6f38fd5684db2b82881ae280
김학준 <gkrwns8805@naver.com>
2022-08-02 11:10:08 +0900

    fix: 로그인 관련

--- frontend-vue/src/components/user/LoginForm.vue
+++ frontend-vue/src/components/user/LoginForm.vue
@@ -1,34 +1,52 @@
 <template>
   <form @submit.prevent="login(credentials)">
     <h1>로그인</h1>
-    <BaseInput placeholder="아이디" v-model="credentials.username"></BaseInput>
+    <BaseInput placeholder="아이디" v-model="credentials.user_id"></BaseInput>
     <BaseInput placeholder="비밀번호" type="password" v-model="credentials.password"></BaseInput>
     <div class="login-nav">
       <router-link to="/users/findid">아이디 찾기</router-link>|
       <router-link to="/users/findpw">비밀번호 찾기</router-link>|
       <router-link to="/users/regist">회원가입</router-link>
     </div>
-    <BaseButton @click="onSubmit">로그인</BaseButton>
+    <kakaoLogin></kakaoLogin>
+
+    <GoogleLogin class="google-login"></GoogleLogin>
+    <BaseButton @click="submit">로그인</BaseButton>
   </form>
 </template>
 
 <script>
 import BaseButton from '../common/BaseButton.vue'
 import BaseInput from '../common/BaseInput.vue'
-import { mapActions } from 'vuex'
+import kakaoLogin from '@/components/user/KakaoLogin.vue'
+import GoogleLogin from '@/components/user/GoogleLogin.vue'
 import { reactive } from 'vue'
+import axios from 'axios'
+import drf from '@/api/drf'
 
 export default {
-  components: { BaseButton, BaseInput },
+  components: { BaseButton, BaseInput, kakaoLogin, GoogleLogin },
 
   setup () {
-    const credentials = reactive({ username: '', password: '' })
+    const credentials = reactive({
+      user_id: '',
+      password: ''
+    })
 
-    const LoginAction = mapActions(['login'])
-
-    return { LoginAction, credentials }
+    const submit = () => {
+      axios.post({
+        url: drf.accounts.login(),
+        method: 'post',
+        data: credentials
+      }).then((res) => {
+        console.log(res)
+        window.alert('로그인')
+      })
+    }
+    return {
+      credentials, submit
+    }
   }
-
 }
 </script>
 
@@ -40,7 +58,8 @@ form {
   flex-direction: column;
   gap: 10px;
   padding: 48px;
-  background-color: rgba(255, 255, 255, 0.8);
+  background-color: grey;
+  /* background-color: rgba(255, 255, 255, 0.8); */
   backdrop-filter: blur(4px);
 }
 
