0396c50013982ceb6dcd305f826cedf9c6253aa2
SSAFY <dang2013@naver.com>
2022-08-10 09:51:57 +0900

    feat: 코칭이력 수정 중

--- backend-django/GX/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GX'
--- backend-django/PX/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PX'
