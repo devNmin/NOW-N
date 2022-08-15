6fcdc0ab3e136b93c6e16b67db66e260ad306e83
hohyun <hohyun323@naver.com>
2022-08-16 00:23:25 +0900

    etc: CI/CD test

--- backend-django/BE/settings.py
+++ backend-django/BE/settings.py
@@ -90,23 +90,23 @@ WSGI_APPLICATION = 'BE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nown',
+        'USER': 'HH',
+        'PASSWORD': 'Teams_08',
+        'HOST': 'i7b108.p.ssafy.io',
+        'PORT': '3306',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'nown',
-#         'USER': 'root',
-#         'PASSWORD': 'ssafy',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
